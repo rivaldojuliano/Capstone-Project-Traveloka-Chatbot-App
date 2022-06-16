7acf0ddced4088cd6421d0e9f9e16d91bcb1c62f
rivaldojuliano <vlado11jr@gmail.com>
2022-06-16 15:40:27 +0700

    Make Custom  SplashScreen

--- app/src/main/java/com/rivzdev/chatbotapp/ui/home/HomePage.kt
+++ app/src/main/java/com/rivzdev/chatbotapp/ui/home/HomePage.kt
@@ -3,7 +3,6 @@ package com.rivzdev.chatbotapp.ui.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rivzdev.chatbotapp.databinding.ActivityHomePageBinding
 import com.rivzdev.chatbotapp.ui.chat.ChatActivity
 
@@ -12,7 +11,6 @@ class HomePage : AppCompatActivity() {
     private lateinit var binding: ActivityHomePageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/rivzdev/chatbotapp/utils/Constant.kt
+++ app/src/main/java/com/rivzdev/chatbotapp/utils/Constant.kt
@@ -2,6 +2,5 @@ package com.rivzdev.chatbotapp.utils
 
 object Constant {
     const val TAG = "ChatBotViewModel"
-    const val  SEND_ID = "SEND_ID"
-    const val RECEIVED_ID = "RECEIVED_ID"
+    const val time = 4000L
 }
